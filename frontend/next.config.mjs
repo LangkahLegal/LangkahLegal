e7c310603c9d2aa7034b9966a2f7121f94dbf097
OdeChaerani <zachra1717@gmail.com>
2026-04-11 19:03:45 +0700

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,13 +2,7 @@
 const nextConfig = {
   reactCompiler: true,
   reactStrictMode: false,
-  // transpilePackages membantu jika ada masalah import library
-  transpilePackages: ['lightningcss'],
   serverExternalPackages: ['lightningcss'],
-  experimental: {
-    // Biarkan kosong dulu
-    turbo: {},
-  },
 };
 
 export default nextConfig;
